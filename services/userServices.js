916dde78656c3f11aa85f4ab3ebb9eba18d0533f
chiragkanani <chiragkanani144@gmail.com>
2023-06-14 16:21:12 +0530

    login API 1

--- services/userServices.js
+++ services/userServices.js
@@ -1,35 +1,42 @@
 var KiteConnect = require("kiteconnect").KiteConnect;
 const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, './config/.env') })
+require('dotenv').config({path:path.join(__dirname,'./config/.env')})
 module.exports = {
-  login: () => {
-    return new Promise(async (res, rej) => {
-      try {
+    login:() => {
+        return new Promise(async (res,rej) => {
+            try {
+               
 
+var kc = new KiteConnect({
+  api_key: process.env.API_KEY,
+});
 
-        var kc = new KiteConnect({
-          api_key: process.env.API_KEY,
-        });
+kc.generateSession("otBfwVvJNuPC23z8USs7T26IHsVRaxCM", process.env.API_SECRET)
+  .then(function (response) {
+    init();
+    res({data:response})
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
-        kc.generateSession(process.env.REQUEST_TOKEN, process.env.API_SECRET)
-          .then(function (response) {
-            init();
-            // console.log("Result 1--->",response);
-            res({ data: response })
-          })
+function init() {
 
-        function init() {
-          // Fetch equity margins.
-          // You can have other api calls here.
-          kc.getMargins()
-            .then(function (response) {
-              res({ EquityMargins: response, message: "Success..!!!" })
-            })
-        }
-      } catch (error) {
-        console.log(error);
-        // rej({status:500,message:"Something Went Wrong..!!!"})
-      }
+  kc.getMargins()
+    .then(function (response) {
+ 
+       res({result:response,message:"Success...!!!!"}) 
+
     })
-  }
+    .catch(function (err) {
+        console.log("Error");
+
+    });
+}
+            } catch (error) {
+                console.log(error);
+                rej({status:500,message:"Something Went Wrong..!!!"})
+            }
+        })
+    }
 }
