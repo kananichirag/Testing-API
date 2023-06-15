a9e73a6ca8c1dd338e9f352b6a1f110f8e5090a5
chiragkanani <chiragkanani144@gmail.com>
2023-06-15 09:47:35 +0530

    Instrument  API

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,6 +4,7 @@ const UserController = require('../controller/LoginController')
 const LoginRoutes = require('../routes/loginRoutes')
 const WsRoutes = require('../routes/wsRoutes')
 const PortfolioRoutes = require('../routes/portfolioRoutes')
+const InstrumentRoutes = require('../routes/instrumentRoutes')
 
 v1.get('/',(req,res) => {
     res.send("Index Route is Working..!!")
@@ -13,5 +14,6 @@ v1.get('/',(req,res) => {
 v1.use('/user',LoginRoutes)
 v1.use('/ws',WsRoutes)
 v1.use('/portfolio',PortfolioRoutes)
+v1.use('/instrument',InstrumentRoutes)
 
 module.exports = v1;
---
+++ services/instrumentServices.js
@@ -0,0 +1,121 @@
+const path = require('path')
+const axios = require('axios')
+require('dotenv').config({ path: path.join(__dirname, './config/.env') })
+
+
+
+module.exports = {
+
+    InstrumentList: () => {
+        return new Promise(async (res, rej) => {
+            try {
+
+                axios.get('https://api.kite.trade/instruments', {
+                    headers: {
+                        "X-Kite-Version": "3",
+                        "Authorization": `token ${process.env.API_KEY}:${process.env.ACC_TOKEN}`
+                    }
+                })
+                    .then(response => {
+
+                        res(response)
+                        // console.log(response.data);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+
+            } catch (error) {
+                console.log(error);
+                rej({ status: 500, message: "Something Went Wrong..!!!" })
+            }
+        })
+    },
+
+
+
+    FindByInstrument: (Name) => {
+        return new Promise(async (res, rej) => {
+            try {
+                const name = Name.toUpperCase();
+                axios.get(`https://api.kite.trade/quote?i=NSE:${name}`, {
+                    headers: {
+                        "X-Kite-Version": "3",
+                        "Authorization": `token ${process.env.API_KEY}:${process.env.ACC_TOKEN}`
+                    }
+                })
+                    .then(response => {
+
+                        res(response)
+                        // console.log(response.data);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+
+            } catch (error) {
+                console.log(error);
+                rej({ status: 500, message: "Something Went Wrong..!!!" })
+            }
+        })
+    },
+
+
+
+    ohlc: (Name) => {
+        return new Promise(async (res, rej) => {
+            try {
+                const name = Name.toUpperCase();
+                axios.get(`https://api.kite.trade/quote/ohlc?i=${name}`, {
+                    headers: {
+                        "X-Kite-Version": "3",
+                        "Authorization": `token ${process.env.API_KEY}:${process.env.ACC_TOKEN}`
+                    }
+                })
+                    .then(response => {
+
+                        res(response)
+                        // console.log(response.data);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+
+            } catch (error) {
+                console.log(error);
+                rej({ status: 500, message: "Something Went Wrong..!!!" })
+            }
+        })
+    },
+
+
+
+    
+    LTP: (Name) => {
+        return new Promise(async (res, rej) => {
+            try {
+                const name = Name.toUpperCase();
+                axios.get(`https://api.kite.trade/quote/ltp?i=${name}`, {
+                    headers: {
+                        "X-Kite-Version": "3",
+                        "Authorization": `token ${process.env.API_KEY}:${process.env.ACC_TOKEN}`
+                    }
+                })
+                    .then(response => {
+
+                        res(response)
+                        // console.log(response.data);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+
+            } catch (error) {
+                console.log(error);
+                rej({ status: 500, message: "Something Went Wrong..!!!" })
+            }
+        })
+    }
+
+
+}
